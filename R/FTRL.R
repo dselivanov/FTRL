b8d8685e11fb381bd55e8c09a5d7a775b6b41b94
Dymitriy Selivanov <selivanov.dmitriy@gmail.com>
2016-12-28 09:12:10 +0400

    initial release

---
+++ R/FTRL.R
@@ -0,0 +1,213 @@
+#' @import methods
+#' @import Matrix
+#' @import Rcpp
+#' @importFrom R6 R6Class
+#' @importFrom utils txtProgressBar setTxtProgressBar
+#' @useDynLib FTRL
+
+init_ftrl_param = function(x, n_features) {
+  init = numeric(n_features)
+  if(!is.null(x)) {
+    stopifnot(length(x) != n_features || is.numeric(x))
+    init = x
+  }
+  init
+}
+
+#' @export
+FTRL = R6::R6Class(
+  classname = "estimator",
+  public = list(
+    #-----------------------------------------------------------------
+    initialize = function(alpha, beta,
+                          lambda1, lambda2,
+                          n_features,
+                          z = NULL, n = NULL) {
+      private$z = init_ftrl_param(z, n_features)
+      private$n = init_ftrl_param(n, n_features)
+
+      private$alpha = alpha
+      private$beta = beta
+
+      private$lambda1 = lambda1
+      private$lambda2 = lambda2
+
+      private$n_features = n_features
+
+      private$model_ptr = create_ftrl_model(private$z,
+                                            private$n,
+                                            alpha = alpha,
+                                            beta = beta,
+                                            lambda1 = lambda1,
+                                            lambda2 = lambda2,
+                                            n_features = n_features)
+    },
+    #-----------------------------------------------------------------
+    dump = function() {
+      model_dump = list(alpha = private$alpha, beta = private$beta,
+                        lambda1 = private$lambda1, lambda2 = private$lambda2,
+                        z = private$z, n = private$n,
+                        n_features = private$n_features)
+      class(model_dump) = "ftrl_model_dump"
+      model_dump
+    },
+    #-----------------------------------------------------------------
+    load = function(x) {
+      if(class(x) != "ftrl_model_dump")
+        stop("input should be class of 'ftrl_model_dump' -  list of model parameters")
+      self$initialize(alpha = x$alpha, beta = x$beta,
+                      lambda1 = x$lambda1, lambda2 = x$lambda2,
+                      n_features = x$n_features,
+                      z = x$z, n = x$n)
+    },
+    #-----------------------------------------------------------------
+    partial_fit = function(X, y, ...) {
+      stopifnot(inherits(X, "sparseMatrix"))
+      if(!inherits(class(X), private$internal_matrix_format)) {
+        # message(Sys.time(), " casting input matrix to ", private$internal_matrix_format)
+        X = as(X, private$internal_matrix_format)
+      }
+      stopifnot(ncol(X) == private$n_features)
+      stopifnot(nrow(X) == length(y))
+      if(any(is.na(X)))
+        stop("NA's in input matrix are not allowed")
+      # stopifnot(is.na(X)))
+      p = ftrl_partial_fit(m = X, y = y, ptr = private$model_ptr, do_update = TRUE)
+      private$n = p$n
+      private$z = p$z
+      invisible(p$pred)
+    },
+    #-----------------------------------------------------------------
+    predict = function(X, ...) {
+      stopifnot(inherits(X, "sparseMatrix"))
+      if(!inherits(class(X), private$internal_matrix_format)) {
+        # message(Sys.time(), " casting input matrix to ", private$internal_matrix_format)
+        X = as(X, private$internal_matrix_format)
+      }
+      stopifnot(ncol(X) == private$n_features)
+      if(any(is.na(X)))
+        stop("NA's in input matrix are not allowed")
+      p = ftrl_partial_fit(m = X, y = numeric(0), ptr = private$model_ptr, do_update = FALSE)
+      return(p$pred);
+    }
+  ),
+  private = list(
+    internal_matrix_format = "RsparseMatrix",
+    z = NULL,
+    n = NULL,
+    alpha = NULL,
+    beta = NULL,
+    lambda1 = NULL,
+    lambda2 = NULL,
+    n_features = NULL,
+    model_ptr = NULL
+  )
+)
+
+
+# Reference R implementation
+# quite optimized, only 7-15x slower
+FTRL_R = function(alpha, beta, lambda1, lambda2, nfeature, z = NULL, n = NULL) {
+  z = init_ftrl_param(z, n_features)
+  n = init_ftrl_param(n, n_features)
+  alpha = alpha
+  beta = beta
+  lambda1 = lambda1
+  lambda2 = lambda2
+  ##########################################################################################
+  sigmoid = function(x) {
+    1 / (1 + exp(-x))
+  }
+  ##########################################################################################
+  w_ftprl = function(i) {
+    retval = numeric(length(i))
+    # index = which(abs(z[i]) > lambda1)
+    index = abs(z[i]) > lambda1
+    j = i[index]
+    z_j = z[j]
+    n_j = n[j]
+    retval[index] = - (z_j - sign(z_j) * lambda1) / (lambda2 + (beta + sqrt(n_j)) / alpha)
+    retval
+  }
+  ##########################################################################################
+  predict_internal = function(j, x) {
+    w = w_ftprl(j)
+    # print(w)
+    sigmoid(crossprod(x, w)[[1L]])
+  }
+  ##########################################################################################
+
+  partial_fit = function(x, y, with_pb = interactive()) {#x_cv = NULL, y_cv = NULL, check_each_n = 1e5, j = 1:1e5 ) {
+
+    # cv_train_n = length(j)
+    # x_cv_train = x[, j]
+    p = numeric(ncol(x))
+    if (with_pb)
+      pb = txtProgressBar(max = ncol(x), style = 3)
+
+    for(col in seq_len(ncol(x))) {
+      # if(col %% 1e4 == 0) {
+      # message(paste(Sys.time(), "sample", col))
+      # }
+      index =
+        if (x@p[[col]] == x@p[[col + 1L]]) integer(0)
+      else seq.int(x@p[[col]], x@p[[col + 1L]] - 1L, by = 1L)
+
+      i = x@i[index + 1L] + 1L
+      xx = x@x[index + 1L]
+
+      p[[col]] = predict_internal(i, xx)
+      # if(col %% check_each_n == 0)
+      # message(p[[col]])
+      n_i = n[i]
+      z_i = z[i]
+
+      g = (p[[col]] - y[[col]]) * xx
+      n_i_g2 = n_i + g * g
+      s = (sqrt(n_i_g2) - sqrt(n_i)) / alpha
+
+      z[i] <<- z_i + g - s * w_ftprl(i)
+      n[i] <<- n_i_g2
+      # print(z)
+      # if(col %% check_each_n == 0 && !is.null(x_cv)) {
+      #   if(!is.null(x_cv) && !is.null(y_cv)) {
+      #     cv_score = round(glmnet::auc(y = y_cv, prob = predict(x_cv, FALSE)), 4)
+      #     train_score = round(glmnet::auc(y = y[j], prob = predict(x_cv_train, FALSE)), 4)
+      #     message(Sys.time(), " ", col, " - ", "cv = ", cv_score, " train = ", train_score)
+      #   }
+      # }
+
+      if (with_pb)
+        setTxtProgressBar(pb, col)
+    }
+    if (with_pb)
+      close(pb)
+    list(p = p, z = z, n = n)
+  }
+  predict = function(x, with_pb = interactive(), check = 10) {
+    p = numeric(ncol(x))
+
+    if (with_pb)
+      pb = txtProgressBar(max = ncol(x), style = 3)
+
+    for(col in seq_len(ncol(x))) {
+      index =
+        if (x@p[[col]] == x@p[[col + 1L]])
+          integer(0)
+      else
+        seq.int(x@p[[col]], x@p[[col + 1L]] - 1L, by = 1L)
+
+      i = x@i[index + 1L] + 1L
+      xx = x@x[index + 1L]
+      p[[col]] = predict_internal(i, xx)
+      # if(col %% check == 0)
+      #   message(p[[col]])
+      if (with_pb)
+        setTxtProgressBar(pb, col)
+    }
+    if (with_pb)
+      close(pb)
+    p
+  }
+  list(predict = predict, partial_fit = partial_fit)
+}
---
+++ src/ftrl.cpp
@@ -0,0 +1,120 @@
+#include <Rcpp.h>
+#include <cmath>
+using namespace Rcpp;
+using namespace std;
+#define N_CHECK_USER_INTERRUPT 100000
+
+inline double sigmoid(double x) {
+  return(1 / (1 + exp(-x)));
+}
+
+inline float sign(double x) {
+  if (x > 0) return 1.0;
+  if (x < 0) return -1.0;
+  return 0.0;
+}
+
+class ftrl_model {
+public:
+  ftrl_model(NumericVector z_inp, NumericVector n_inp, double alpha, double beta, double lambda1, double lambda2, int n_features):
+  alpha(alpha), beta(beta), lambda1(lambda1), lambda2(lambda2), n_features(n_features) {
+    z = as< vector<double> > (z_inp);
+    n = as< vector<double> > (n_inp);
+  }
+  vector<double> z;
+  vector<double> n;
+  double alpha;
+  double beta;
+  double lambda1;
+  double lambda2;
+  int n_features;
+};
+
+// [[Rcpp::export]]
+SEXP create_ftrl_model(NumericVector z_inp, NumericVector n_inp, double alpha, double beta, double lambda1, double lambda2, int n_features) {
+  ftrl_model* model = new ftrl_model( z_inp,  n_inp,  alpha,  beta,  lambda1,  lambda2,  n_features);
+  // Rprintf("alpha = %f, beta = %f, lambda1=%f, lambda2=%f, n_features=%d\n",
+  //         model->alpha, model->beta, model->lambda1, model->lambda2,  model->n_features);
+  XPtr< ftrl_model> ptr(model, true);
+  return ptr;
+}
+
+//calculates regression weights for inference
+vector<double> w_ftprl(const vector<int> &nnz_index, const Rcpp::XPtr<ftrl_model> &model) {
+  vector<double> retval(nnz_index.size());
+  int k = 0;
+  for (auto j:nnz_index) {
+    double z_j = model->z[j];
+    if(abs(z_j) > model->lambda1) {
+      double n_j = model->n[j];
+      retval[k] = (-1 / ((model->beta + sqrt(n_j)) / model->alpha  + model->lambda2)) *  (z_j - sign(z_j) * model->lambda1);
+    }
+    k++;
+  }
+  return(retval);
+};
+
+double predict_one(const vector<int> &index, const vector<double> &x, const Rcpp::XPtr<ftrl_model> &model) {
+  // Rprintf("alpha = %f, beta = %f, lambda1=%f, lambda2=%f, n_features=%d\n",
+  //         model->alpha, model->beta, model->lambda1, model->lambda2,  model->n_features);
+  vector<double> weights = w_ftprl(index, model);
+
+  double prod = 0;
+  for(int i = 0; i < index.size(); i++)
+    prod += weights[i] * x[i];
+  double res = sigmoid(prod);
+  return(res);
+}
+
+// [[Rcpp::export]]
+List ftrl_partial_fit(S4 m, NumericVector y, SEXP ptr, int do_update = 1) {
+  Rcpp::XPtr<ftrl_model> model(ptr);
+  // Rprintf("alpha = %f, beta = %f, lambda1=%f, lambda2=%f, n_features=%d\n",
+  //         model->alpha, model->beta, model->lambda1, model->lambda2,  model->n_features);
+  IntegerVector P = m.slot("p");
+  IntegerVector J = m.slot("j");
+  NumericVector X = m.slot("x");
+  IntegerVector dims = m.slot("Dim");
+  int N = dims[0];
+  NumericVector y_hat(N);
+
+  for(int i = 0; i < N; i++) {
+
+    if( i % N_CHECK_USER_INTERRUPT == 0)
+      checkUserInterrupt();
+
+    size_t p1 = P[i];
+    size_t p2 = P[i + 1];
+    int len = p2 - p1;
+    vector<int> example_index(len);
+    vector<double> example_value(len);
+    int k = 0;
+    for(int pp = p1; pp < p2; pp++) {
+      example_index[k] = J[pp];
+      example_value[k] = X[pp];
+      k++;
+    }
+    y_hat[i] = predict_one(example_index, example_value, model);
+
+    if(do_update) {
+      double d = y_hat[i] - y[i];
+      double grad;
+      double n_i_g2;
+      double sigma;
+      vector<double> ww = w_ftprl(example_index, model);
+
+      int k = 0;
+      for(auto ii:example_index) {
+        grad = d * example_value[k];
+        n_i_g2 = model->n[ii] + grad * grad;
+        sigma = (sqrt(n_i_g2) - sqrt(model->n[ii])) / model->alpha;
+        model->z[ii] += grad - sigma * ww[k];
+        model->n[ii] = n_i_g2;
+        k++;
+      }
+    }
+  }
+  return List::create(_["pred"]  = y_hat,
+                      _["z"] = wrap(model->z),
+                      _["n"] = wrap(model->n)) ;
+}
